2c8f5be954fb8ced92fe916484994fbbf164a912
Christopher George <chris@ChristophersMBP.wv.cc.cmu.edu>
2018-08-07 18:12:36 -0400

    updated repos to automatically put numpy arrays into all_image_data and updated numpy arrays to be the same size as the original image

--- image-segmentation.py
+++ image-segmentation.py
@@ -1,13 +1,37 @@
-import pickle
+import numpy
 
 # This file will retreive the mask data from running the deeplab-pytorch.
 # Then it will take that data, and spit merge the depth-mapping information.
 
-# data is a dict of with the key being the label of a class, and the
+# segmentedData is a dict of with the key being the label of a class, and the
 # value being the numpy array of the mask for the image.
-data = dict()
-dictPath = './deeplab-pytorch/test_images/test.txt'
+segmentedData = dict()
+dictPath = './all_image_data/school/school_segmentation_array.npy'
 
 with open(dictPath, 'rb') as file:
-    data = pickle.load(file, fix_imports=True, encoding="ASCII", errors="strict")
+    segmentedData = numpy.load(file, fix_imports=True, encoding="ASCII")
+
+# depth is a numpy array of the depth of the image, the size of the image
+depthData = None
+dictPath = './all_image_data/school/school_depth_array.npy'
+
+with open(dictPath, 'rb') as file:
+    depthData = numpy.load(file, fix_imports=True, encoding="ASCII")
+
+# print(segmentedData)
+# print(depthData)
+
+print("depth rows", len(depthData.tolist()))
+print("depth cols", len(depthData.tolist()[0]))
+segmentedData = dict(numpy.ndenumerate(segmentedData))
+
+# because of weird numpy translation
+for fakeKey in segmentedData:
+    trueSegmentedData = segmentedData[fakeKey]
+    for realKey in trueSegmentedData:
+        print("segmented cols", len(trueSegmentedData[realKey].tolist()))
+        print("segmented rows", len(trueSegmentedData[realKey].tolist()[0]))
+        break
+    break
+
 
